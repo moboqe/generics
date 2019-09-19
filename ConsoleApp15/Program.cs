797d907260b6dd12515aef6d906532ffaf55ff25
Max <generalmaxis@yandex.ru>
2019-09-20 02:20:14 +0500

    3

--- ConsoleApp15/Program.cs
+++ ConsoleApp15/Program.cs
@@ -72,50 +72,43 @@ namespace ConsoleApp15
             Mnozhestvo<double> arr4 = new Mnozhestvo<double>();
             Mnozhestvo<string> arr5 = new Mnozhestvo<string>();
             Mnozhestvo<string> arr6 = new Mnozhestvo<string>();
-            string[] text = new string[30];
-            text[0] = "a"; text[1] = "b"; text[2] = "c"; text[3] = "d"; text[4] = "e";
-            text[5] = "f"; text[6] = "g"; text[7] = "h"; text[8] = "i"; text[9] = "j";
-            text[10] = "k"; text[11] = "l"; text[12] = "m"; text[13] = "n"; text[14] = "o";
-            text[15] = "p"; text[16] = "q"; text[17] = "r"; text[18] = "s"; text[19] = "t";
-            text[20] = "u"; text[21] = "v"; text[22] = "w"; text[23] = "x"; text[24] = "y"; text[25] = "z";
-         
 
+            string letters = "abcdefghijklmnopqrstuvwxyz";
 
-
-            range = rnd.Next(5, 10);
+            range = rnd.Next(5, 10);       // длина первого множества
             for (int i = 0; i < range; i++)
             {
                 arr1.Add(rnd.Next(-10, 11));
             } 
 
-            range = rnd.Next(5, 10);
+            range = rnd.Next(5, 10);       // длина второго множества
             for (int i = 0; i < range; i++)
             {
                 arr2.Add(rnd.Next(-10, 11));
             }
 
-            range = rnd.Next(5, 10);
+            range = rnd.Next(5, 10);       // длина третьего множества 
             for (int i = 0; i < range; i++)
             {
                 arr3.Add(Convert.ToDouble(rnd.Next(-10, 11) / 10d));
             }
 
-            range = rnd.Next(5, 10);
+            range = rnd.Next(5, 10);       // длина четвертого множества
             for (int i = 0; i < range; i++)
             {
                 arr4.Add(Convert.ToDouble(rnd.Next(-10, 11) / 10d));
             }
            
-            range = rnd.Next(5, 10);
+            range = rnd.Next(5, 10);       // длина пятого множества
             for (int i = 0; i < range; i++)
             {
-                arr5.Add(text[rnd.Next(0, text.Length)]);
+                arr5.Add(Convert.ToString(letters[rnd.Next(0, letters.Length)]));
             }
 
-            range = rnd.Next(5, 10);
+            range = rnd.Next(5, 10);       // длина шестого множества
             for (int i = 0; i < range; i++)
             {
-                arr6.Add(text[rnd.Next(0, text.Length)]);
+                arr6.Add(Convert.ToString(letters[rnd.Next(0, letters.Length)]));
             }
 
             Console.Write("Целочисленные множества \n");
